d7792cd704f5dd142360cff7ccd6f3d83a2e0d6c
Asamu victor <asamuvictor03@gmail.com>
2022-10-01 19:56:41 +0100

    signup validation

--- src/components/UserNavBar.js
+++ src/components/UserNavBar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import icon1 from "../images/images__1_-removebg-preview.png"
 import "../styles/UserNavBar.css"
 import icon2 from "../images/information-circle-outline.svg"
+import icon11 from"../images/search-outline.svg"
 import { Link } from 'react-router-dom'
 
 const UserNavBar = () => {
@@ -11,7 +12,7 @@ const UserNavBar = () => {
      <h3 className='vmart-class-logo pt-3 vmart-class-logo2'>Vmart<img src={icon1} alt="" className='admin-logo-img' width={50}/></h3>
      <div className='nav-search-input-div'>
         <input type="text" className='form-control nav-search-input' placeholder='Search products,brand etc'/>
-        <button className='btn btn-info ms-2 bbb'>Search</button>
+        <button className='btn btn-info ms-2  bbb'><img src={icon11} alt="" width={20} /></button>
         <div class="ps-5 ms-5 pt-1 help">
                 <img src={icon2} alt="" width="30"/>
                 <div class="dropdown ps-2 fs-5">
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,65 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import { Link } from 'react-router-dom';
 import "../styles/Signup.css"
 const Signup = () => {
+  const [firstname, setfirstname] = useState("");
+  const [lastname, setlastname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [firstnameerror, setfirstnameerror] = useState("");
+  const [lastnameerror, setlastnameerror] = useState("");
+  const [emailerror, setemailerror] = useState("");
+  const [passworderror, setpassworderror] = useState("");
+  const signup=()=>{
+    let regexForFirstName=/^[\w]{3,}$/
+    let regexForLastName=/^[\w]{3,}$/
+    let regexForEmail=/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/
+    let regexForPassword=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/
+    if (firstname==""||lastname==""||email==""||password==""&&!regexForFirstName.test(firstname)||!regexForLastName.test(lastname)||!regexForEmail.test(email)||!regexForPassword.test(password)
+    ) {
+      if(firstname==""){
+        setfirstnameerror("please this field is required !!")
+      }else if(!regexForFirstName.test(firstname)){
+        setfirstnameerror("firstname must be at least 3 character and space is not allowed !!") 
+      }else if(!firstname==""||regexForFirstName.test(firstname)){
+        setfirstnameerror("")
+      }
+      if(lastname==""){
+        setlastnameerror("please this field is required !!")
+      }else if(!regexForLastName.test(lastname)){
+        setlastnameerror("lastname must be at least 3 character and space is not allowed !!") 
+      }else if(!lastname==""||regexForLastName.test(firstname)){
+        setlastnameerror("")
+      }
+      if(email==""){
+        setemailerror("please this field is required !!")
+      }else if(!regexForEmail.test(email)){
+        setemailerror("please enter a correct email format !!") 
+      }else if(!email==""||regexForEmail.test(email)){
+        setemailerror("")
+      }
+      if(password==""){
+        setpassworderror("please this field is required !!")
+      }else if(!regexForPassword.test(password)){
+        setpassworderror("password must contain uppercase,lowercase and numbers !!") 
+      }else if(!password==""||regexForPassword.test(password)){
+        setpassworderror("")
+      }
+   }
+    else{
+      setfirstnameerror("")
+      setlastnameerror("")
+      setemailerror("")
+      setpassworderror("")
+      // let signUpObj={firstname,lastname,email,password,status:false}
+      // axios.post(endPoints,signUpObj).then((result)=>{
+      //   console.log(result)
+      // }).catch((err)=>{
+      //   console.log(err)
+      // });
+      alert("correct login")
+  }
+  }
   return (
     <>
      <center>
@@ -13,11 +71,15 @@ const Signup = () => {
           </div>
           <div className='second-signup-div'>
             <h1 className='create-account-text-h1'>Create new account</h1>
-            <input type="text" placeholder='firstname' className='form-control mb-4 w-75'/>
-            <input type="text" placeholder='lastname'className='form-control mb-4 w-75'/>
-            <input type="email" placeholder='email'className='form-control mb-4 w-75'/>
-            <input type="password" placeholder='password'className='form-control mb-4 w-75'/>
-            <button className='btn btn-info w-75'>Signup</button>
+            <input type="text" placeholder='firstname' className='form-control mb-4 w-75' onChange={(e)=>setfirstname(e.target.value)} value={firstname}/>
+            <div className='text-danger'>{firstnameerror}</div>
+            <input type="text" placeholder='lastname'className='form-control mb-4 w-75' onChange={(e)=>setlastname(e.target.value)} value={lastname}/>
+            <div className='text-danger'>{lastnameerror}</div>
+            <input type="email" placeholder='email'className='form-control mb-4 w-75' onChange={(e)=>setemail(e.target.value)} value={email}/>
+            <div className='text-danger'>{emailerror}</div>
+            <input type="password" placeholder='password'className='form-control mb-4 w-75' onChange={(e)=>setpassword(e.target.value)} value={password}/>
+            <div className='text-danger'>{passworderror}</div>
+            <button className='btn btn-info w-75' onClick={signup}>Signup</button>
           </div>
         </div>
      </center>
