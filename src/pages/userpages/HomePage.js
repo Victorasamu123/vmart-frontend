f21ea5ef1006ea465ac24538316eb81187983b4a
Asamu victor <asamuvictor03@gmail.com>
2022-10-04 22:37:44 +0100

    started userhome page

--- src/pages/userpages/HomePage.js
+++ src/pages/userpages/HomePage.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom';
 import Footer from '../../components/Footer';
 import HomeNav from '../../components/HomeNav';
 import UserNavBar from '../../components/UserNavBar';
-
+import UserHomePage from './UserHomePage';
 const HomePage = () => {
   return (
     <>
       <UserNavBar/>
       <HomeNav/>
-      <Footer/>
+      <Routes>
+        <Route path='/' element={<UserHomePage/>}/>
+      </Routes>
     </>
   )
 }
---
+++ src/pages/userpages/UserHomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserHomePage = () => {
+  return (
+    <>
+     <h1>hello user mi</h1>
+    </>
+  )
+}
+
+export default UserHomePage;
