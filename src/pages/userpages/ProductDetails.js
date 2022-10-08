b43a06761d88fe6f28e7b3ff59d90581971891e6
Asamu victor <asamuvictor03@gmail.com>
2022-10-08 20:22:19 +0100

    product details style

--- src/pages/userpages/ProductDetails.js
+++ src/pages/userpages/ProductDetails.js
@@ -4,27 +4,48 @@ import { useLocation } from 'react-router-dom';
 
 const ProductDetails = () => {
     const location = useLocation()
-    // const [item_id, setitem_id] = useState('')
-    // const [itemcategory, setitemcategory] = useState('')
+    const [loading, setloading] = useState(false);
+    const [message, setmessage]= useState("");
+    const [productimage, setproductimage]= useState("");
+    const [productname, setproductname]= useState("");
+    const [productcategory, setproductcategory]= useState("");
+    const [productdescription, setproductdescription]= useState("");
+    const [productprice, setproductprice]= useState("");
+    const [productdiscount, setproductdiscount]= useState("");
     const productdetailendpoints= "http://localhost:4000/addproducts/productdetails"
     useEffect(() => {
      console.log(location.state)
-     setitem_id()
-     setitemcategory(location.state.itemcategory);
-     let item_id=location.state.item_id
      getdetails()
     }, [])
-     
+    
     const getdetails=()=>{
+      setmessage("");
+      setloading(true);
+      let itemcategory=location.state.itemcategory;
+      let item_id=location.state.item_id;
       let Productdetails={item_id,itemcategory};
        axios.post(productdetailendpoints,Productdetails).then((result)=>{
         console.log(result)
+        setloading(false);
+        setmessage(result.data.message);
+        setproductimage(result.data.product.productimage)
+        setproductname(result.data.product.productname)
+        setproductcategory(result.data.product.productcategory)
+        setproductdescription(result.data.product.productdescription)
+        setproductprice(result.data.product.productprice)
+        setproductdiscount(result.data.product.productdiscount)
        })
     }
   return (
     <>
-     <div>{item_id}</div>
-     <div>{itemcategory}</div>
+      {message==""?<div class="d-flex justify-content-center">
+            <div class="spinner-border text-primary" role="status">
+              <span class="sr-only"></span>
+            </div>
+          </div>
+          :<div>
+            
+          </div>}
     </>
   )
 }
