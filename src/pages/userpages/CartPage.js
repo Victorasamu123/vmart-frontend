05e6ab7af67d4eb625a802965037801e0fb34380
Asamu victor <asamuvictor03@gmail.com>
2022-10-17 22:51:06 +0100

    cart page update

--- src/pages/userpages/CartPage.js
+++ src/pages/userpages/CartPage.js
@@ -7,12 +7,16 @@ const CartPage = () => {
   const [message, setmessage]= useState("");
   const [cartArray, setcartArray]= useState([]);
   const [saveitems,setsaveitems]= useState("");
+  const [saveitemsArray,setsaveitemsArray]= useState([]);
   const userId=localStorage.userId
   const cartendpoints="http://localhost:4000/cart/getcart"
   const saveitemendpoints = "http://localhost:4000/cart/saveitem"
   const deleteitemendpoints = "http://localhost:4000/cart/removeitem"
+  const getsaveitendpoints = "http://localhost:4000/cart/getsaveitem"
+  const addtoendpoints2= "http://localhost:4000/cart/addtocart2"
   useEffect(() => {
     getcart()
+    getsaveitem()
   }, [])
   const getcart=()=>{
     setmessage("");
@@ -23,6 +27,12 @@ const CartPage = () => {
       setloading(false);
       setmessage(result.data.message);
       setcartArray(result.data.cartresult.reverse())
+    })
+  }
+  const getsaveitem=()=>{
+    let getsaveitemObj={userId}
+    axios.post(getsaveitendpoints,getsaveitemObj).then((result)=>{
+      setsaveitemsArray(result.data.saveitemresult)
     })
   }
   const proceedtopayment=(index)=>{
@@ -43,6 +53,7 @@ const CartPage = () => {
       console.log(result.data.message);
       setsaveitems(result.data.message);
       getcart()
+      getsaveitem()
     })
   }
   const removeitem=(index)=>{
@@ -60,23 +71,46 @@ const CartPage = () => {
       console.log(result.data.message);
       setsaveitems(result.data.message);
       getcart()
+      getsaveitem()
+    })
+  }
+  const addtocart2=(index)=>{
+    let filteredArray = saveitemsArray.filter((item, ind) => index == ind);
+    console.log(filteredArray)
+    let productimage=filteredArray[0].productimage
+    let productname=filteredArray[0].productname
+    let productcategory=filteredArray[0].productcategory
+    let productdescription=filteredArray[0].productdescription
+    let productprice=filteredArray[0].productprice
+    let productdiscount=filteredArray[0].productdiscount
+    let userId=localStorage.userId
+    let cartObj={productimage,productname,productcategory,productdescription,productprice,productdiscount,userId}
+    axios.post(addtoendpoints2,cartObj).then((result)=>{
+      console.log(result.data.message);
+      getcart()
+      getsaveitem()
     })
   }
   return (
     <>
-      {message==""?<div className="d-flex justify-content-center">
+      {message===""?<div className="d-flex justify-content-center">
             <div className="spinner-border text-primary" role="status">
               <span className="sr-only"></span>
             </div>
           </div>
           :
+          <div>
           <div className='user-home-page-second-div mt-2 pb-2'>
             <div className='w-100 row'>
               <h1 className='col-lg-12 col-md-12 col-sm-12'>
                <center>Cart Page</center>
                </h1>
             </div>
-          <div className='general-div-product-details-page'>
+        </div>
+
+            {cartArray==""?<div className='fs-1'><center>cart page is currently empty, add product to view in cart page</center></div>
+              :
+              <div className='general-div-product-details-page'>
             {cartArray.map((cart,index)=>(
               <div className='w-100 row' key={cart._id}>
               <div className='col-lg-6 col-md-12 col-sm-12'>
@@ -117,6 +151,41 @@ const CartPage = () => {
             ))
             }
         </div>
+            }
+           <div className='user-home-page-second-div mt-5 pb-2'>
+     <div className='w-100 row'>
+      <h4 className='ms-3 col-lg-12 col-md-12 col-sm-12'>Saved Items</h4>
+          <div className='row w-100 ms-2'>
+            {saveitemsArray==""?<div className='fs-1'><center>No Saved Items</center></div>:
+              <div>
+                {saveitemsArray.map((saveitem,index)=>(
+              <div className='col-lg-2 col-md-6 col-sm-6 phone-display-div ms-3 mb-2' key={saveitem._id}>
+                <div>
+                 <Link className='link-phone-details'>
+                <center>
+                  <div className='comp-img'>
+                <img src={saveitem.productimage} alt="" className='phones-img1 comp-img'/>
+                  </div>
+                  <div className='phone-name'>
+                <div>{saveitem.productname}</div>
+                  </div>
+                <div className='phone-price'>₦{saveitem.productprice}</div>
+                <div className='phone-price2'>₦{saveitem.productprice}</div>
+                <div className='comp-discount mb-2'>-{saveitem.productdiscount}</div>
+                </center>
+              </Link>
+              </div>
+              <center>
+              <button className='btn mb-3 add-to-cart text-light' style={{backgroundColor:"rgb(251,87,3)"}} onClick={()=>addtocart2(index)}>Add to cart</button>
+              </center>
+              </div>
+            ))}
+              </div>
+            }
+           
+          </div>
+    </div>
+     </div>
         </div>
           }
     </>
