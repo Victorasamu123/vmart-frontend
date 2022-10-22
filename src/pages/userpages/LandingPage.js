8dea5819153b7e1f92839c4910be13609380d30d
Asamu victor <asamuvictor03@gmail.com>
2022-10-22 22:11:36 +0100

    landing page update

--- src/pages/userpages/LandingPage.js
+++ src/pages/userpages/LandingPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import UserNavBar from '../../components/UserNavBar';
 import UserNavBar2 from '../../components/UserNavBar2';
 import "../../styles/LandingPage.css"
@@ -10,8 +10,47 @@ import img5 from "../../images/Tripple_banner.jpg"
 import img6 from "../../images/get_it_on_google_image.svg"
 import img7 from "../../images/download_on_app_store_image.svg"
 import img8 from "../../images/202103050842528033.jpg"
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 const LandingPage = () => {
+  const [loading, setloading] = useState(false);
+  const [message, setmessage]= useState("");
+  const [phoneArrays, setphoneArrays]= useState([]);
+  const [messphone,setmessphone]= useState('')
+  const [messcart,setmesscart]= useState('')
+  const navigate=useNavigate()
+  const getphonesendpoints= "https://vmart-backend.vercel.app/addproducts/getphones"
+  const addtoendpoints= "https://vmart-backend.vercel.app/cart/addtocart"
+  useEffect(() => {
+    getPhones()
+  }, [])
+  
+  const getPhones=()=>{
+    setmessage("")
+    setloading(true);
+    axios.get(getphonesendpoints).then((result)=>{
+      if(result.data.status==true){
+        setloading(false);
+        setmessphone(result.data.message)
+        console.log(result);
+        setphoneArrays(result.data.phonearray.slice(10))
+      }else{
+        setmessage("")
+        setloading(true);
+        setphoneArrays([])
+      }
+    })
+  }
+  const goToDetails=(index)=>{
+    let filteredArray = phoneArrays.filter((item, ind) => index == ind);
+    let itemcategory=filteredArray[0].productcategory
+    let item_id=filteredArray[0]._id
+    console.log(itemcategory,item_id)
+    navigate(`/homepage/${item_id}` , {state:{filteredArray}})
+  }
+  const addtocart=()=>{
+    navigate("/siginin")
+  }
   return (
     <>
      <UserNavBar/>
@@ -45,7 +84,42 @@ const LandingPage = () => {
         </center>
        </div>
      </div>
-     
+     <div className='user-home-page-second-div mt-2 pb-2'>
+     <div className='w-100 row'>
+      <h4 className='ms-3 col-lg-12 col-md-12 col-sm-12'>Top selling Phone Product</h4>
+       {messphone==""?<div class="d-flex justify-content-center">
+            <div className="spinner-border text-primary" role="status">
+              <span className="sr-only"></span>
+            </div>
+          </div>:
+          <div className='row w-100 ms-2'>
+            {phoneArrays.map((phones,index)=>(
+              <div className='col-lg-2 col-md-6 col-sm-6 phone-display-div ms-3 mb-2' key={phones._id}>
+                <div onClick={()=>goToDetails(index)}>
+                 <Link className='link-phone-details'>
+                <center>
+                  <div className='phones-img'>
+                <img src={phones.productimage} alt="" className='phones-img1'/>
+                  </div>
+                  <div className='phone-name'>
+                <div>{phones.productname}</div>
+                  </div>
+                <div className='phone-price'>₦{phones.productprice}</div>
+                <div className='phone-price2'>₦{phones.productprice}</div>
+                <div className='phones-discount mb-2'>-{phones.productdiscount}</div>
+                </center>
+              </Link>
+              </div>
+              <center>
+              <button className='btn mb-3 add-to-cart text-light' style={{backgroundColor:"rgb(251,87,3)"}} onClick={()=>addtocart(index)}>Add to cart</button>
+              </center>
+              </div>
+            ))}
+            <Link to="/homepage/phonecategory">see more phone product</Link>
+          </div>
+       }
+    </div>
+     </div>
     </>
   )
 }
